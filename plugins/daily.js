0f5988477fa1d7945f20beef7256505a88d344f6
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-03-10 17:29:36 +0530

    `Nandhutty-V3`

--- plugins/daily.js
+++ plugins/daily.js
@@ -2,7 +2,7 @@ const free = 500
 const plum = 5000
 
 let handler = async(m, { conn, usedPrefix, isPrems }) => {
-  if (db.data.users[m.sender].level < 1) return await conn.sendButton(m.chat, 'You are still level 0', '© ᴀᴊᴍᴀʟ-ᴀᴄʜᴜ, 'Level up', `${usedPrefix }levelup`, m)
+  if (db.data.users[m.sender].level < 1) return await conn.sendButton(m.chat, 'You are still level 0', '© ᴀᴊᴍᴀʟ-ᴀᴄʜᴜ', 'Level up', `${usedPrefix }levelup`, m)
   let time = db.data.users[m.sender].lastclaim + 86400000
   if (new Date - db.data.users[m.sender].lastclaim < 86400000) throw `You already claimed daily claim today\nWait for ${conn.msToTime(time - new Date())} again`
   db.data.users[m.sender].exp += isPrems ? prem * db.data.users[m.sender].level : free * db.data.users[m.sender].level
