57d2969875de44f373b443bb38fd2f395a32e7b7
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-03-10 17:39:10 +0530

    `Nandhutty-V3`

--- plugins/okick.js
+++ plugins/okick.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, args }) => {
   let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
-  if (!m.mentionedJid[0]) throw 'tag you want to kick`
+  if (!m.mentionedJid[0]) throw 'tag you want to kick'
   let users = m.mentionedJid.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
   for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [user])
 }
