1f73760abcebf242371bd895f75b14d1d1792ae3
SafwanGanz <67726326+SafwanGanz@users.noreply.github.com>
2022-03-13 00:32:41 +0530

    Update update2.js

--- plugins/update2.js
+++ plugins/update2.js
@@ -4,7 +4,7 @@ const { promisify } = require('util')
 const { join } = require('path')
 
 let confirmation = {}
-let repository = 'ariffb25/stikerinbot'
+let repository = 'Ajmal-Achu/Nandhutty-V3'
 let branch = 'main'
 
 async function handler(m, { text }) {
